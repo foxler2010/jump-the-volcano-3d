ed3137ab3ffe9f9a4e59121cb1a6fdeede7ebf71
foxler2010 <drew@drewssite.top>
2021-12-22 18:03:59 -0600

    more ways to manage the inventory! ;)

--- src/top/drewssite/volcano/Inventory.java
+++ src/top/drewssite/volcano/Inventory.java
@@ -20,7 +20,7 @@ public class Inventory {
         
     }
 
-
+    //add item to inventory
     //depending on the type of item, it will go into a different list.
     public void addItem(Item item) {
         
@@ -52,6 +52,7 @@ public class Inventory {
         }
     }
 
+    //return the item at a specified type and index
     public Item getItem(itemType type, int index) {
         
         //initialize as null, because it will throw an error if I don't
@@ -83,6 +84,127 @@ public class Inventory {
 
         //return resulting item
         return requestedItem;
+    }
+
+    //remove and item from the list; based of of the type and index
+    public void removeItem(itemType type, int index) {
+        
+        //use different lists based on type
+        switch (type) {
+            case JUNK:
+            inventory.get(0).remove(index);
+            break;
+            case FOOD:
+            inventory.get(1).remove(index);
+            break;
+            case WEAPON:
+            inventory.get(2).remove(index);
+            break;
+            case PET:
+            inventory.get(3).remove(index);
+            break;
+            case KIT:
+            inventory.get(4).remove(index);
+            break;
+            case MONSTER:
+            inventory.get(5).remove(index);
+            break;
+            case OTHER:
+            inventory.get(6).remove(index);
+        }
+
+    }
+
+    //remove and item from the list; based of of the type and item name
+    public void removeItem(itemType type, Item item) {
+        
+        //use different lists based on type
+        switch (type) {
+            case JUNK:
+            inventory.get(0).remove(item);
+            break;
+            case FOOD:
+            inventory.get(1).remove(item);
+            break;
+            case WEAPON:
+            inventory.get(2).remove(item);
+            break;
+            case PET:
+            inventory.get(3).remove(item);
+            break;
+            case KIT:
+            inventory.get(4).remove(item);
+            break;
+            case MONSTER:
+            inventory.get(5).remove(item);
+            break;
+            case OTHER:
+            inventory.get(6).remove(item);
+        }
+
+    }
+
+    //see if an item is in the player's inventory
+    public boolean checkForItem(Item item) {
+
+        //initialize as false, because it will throw an error if I don't
+        boolean itemIsPresent = false;
+
+        //for every sub-list, check for the item.
+        //if true, return the result immediately so that the boolean won't get messed with by the other if statements.
+
+        //i realize now this could've been done with a for loop. i will implement it that way some other time.
+        if (inventory.get(0).contains(item)) {
+            itemIsPresent = true;
+            return itemIsPresent;
+        } else {
+            itemIsPresent = false;
+        }
+
+        if (inventory.get(1).contains(item)) {
+            itemIsPresent = true;
+            return itemIsPresent;
+        } else {
+            itemIsPresent = false;
+        }
+
+        if (inventory.get(2).contains(item)) {
+            itemIsPresent = true;
+            return itemIsPresent;
+        } else {
+            itemIsPresent = false;
+        }
+
+        if (inventory.get(3).contains(item)) {
+            itemIsPresent = true;
+            return itemIsPresent;
+        } else {
+            itemIsPresent = false;
+        }
+
+        if (inventory.get(4).contains(item)) {
+            itemIsPresent = true;
+            return itemIsPresent;
+        } else {
+            itemIsPresent = false;
+        }
+
+        if (inventory.get(5).contains(item)) {
+            itemIsPresent = true;
+            return itemIsPresent;
+        } else {
+            itemIsPresent = false;
+        }
+
+        if (inventory.get(6).contains(item)) {
+            itemIsPresent = true;
+            return itemIsPresent;
+        } else {
+            itemIsPresent = false;
+        }
+
+        //so that it returns false when the item does not exist anywhere
+        return itemIsPresent;
     }
 
 }

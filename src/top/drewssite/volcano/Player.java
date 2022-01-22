66cc22257a53113ce7dab33fdabb4abe58681852
foxler2010 <drew@drewssite.top>
2022-01-22 10:46:22 -0600

    make Player public, add wrappers, and do some docs
    All methods in Player are now public,
    I have added some new wrapper methods to complement the new stuff in Inventory,
    and there is now javadoc for the constructor method.
    Oh and those parameters in the javadoc for the class are gone.

--- src/top/drewssite/volcano/Player.java
+++ src/top/drewssite/volcano/Player.java
@@ -5,16 +5,10 @@ package top.drewssite.volcano;
  *	This class is special as technically it is an "Item", yet it can hold other Items,
  *	it has a level, money, and among other things you can control it directly.
  *	The class is mainly used to store the player's attributes while giving it Animal properties like the ability to attack
-
-	@author foxler2010
-	@see Animal
-	@see Item
-	@since	v1.0
-	@param	name	The "fancy" name of the Player object. Doesn't do anything currently.
-	@param	strength	The strength of the player in combat. Does not currently do anything.
-	@param	level	The initial experience level of the player. In a regular game this is 0.
-	@param	money	The inital amount of money the player has. This is 0 in a regular game.
-	@param	startingInventory	This is what the player's inventory will contain when they start the game.
+ *	@author foxler2010
+ *	@see Animal
+ *	@see Item
+ *	@since	v1.0
  */
 class Player extends Animal {
 	
@@ -23,8 +17,18 @@ class Player extends Animal {
 	private double money;
 	private Inventory inventory = new Inventory();
 	
-	//CONSTRUCTOR
-	Player(String name, int strength, int level, double money, Inventory startingInventory) {
+	/**
+	 * The constructor for the Player class
+	 * @author foxler2010
+	 * @see Player
+	 * @since v1.0
+	 * @param name	The "fancy" name of the Player object. Doesn't do anything currently.
+	 * @param strength	The strength of the player in combat. Does not currently do anything.
+	 * @param level	The initial experience level of the player. In a regular game this is 0.
+	 * @param money	The inital amount of money the player has. This is 0 in a regular game.
+	 * @param startingInventory	This is what the player's inventory will contain when they start the game.
+	 */
+	public Player(String name, int strength, int level, double money, Inventory startingInventory) {
 		super(name, itemType.OTHER, strength, false);
 		this.level = level;
 		this.money = money;
@@ -32,19 +36,19 @@ class Player extends Animal {
 	}
 	
 	//GETTERS AND SETTERS
-	int getLevel() {
+	public int getLevel() {
 		return level;
 	}
 	
-	double getMoney() {
-		return money;
+	public double getMoney() {
+	return money;
 	}
 	
-	void setLevel(int level) {
+	public void setLevel(int level) {
 		this.level = level;
 	}
 	
-	void setMoney(double money) {
+	public void setMoney(double money) {
 		this.money = money;
 	}
 	
@@ -52,57 +56,65 @@ class Player extends Animal {
 	
 	//Returns an item in the inventory.
 	//Does NOT remove the item from the list.
-	Item getItem(itemType type, int index) {
+	public Item getItem(itemType type, int index) {
 		return inventory.getItem(type, index);
 	}
 	
 	//add an item to the inventory
-	void addItem(Item item) {
+	public void addItem(Item item) {
 		inventory.addItem(item);
 	}
 	
 	//remove something from the inventory
 	//shifts everything in the sub-list down one index if the removed item is not the last index
-	void removeItem(Item item) {
+	public void removeItem(Item item) {
 		inventory.removeItem(item);
 	}
 
-	void removeItem(itemType type, int index) {
+	public void removeItem(itemType type, int index) {
 		inventory.removeItem(type, index);
+	}
+
+	public void removeItem(int typeIndex, int index) {
+		inventory.removeItem(typeIndex, index);
 	}
 	
 	//check if the item is in the inventory
 	//returns either true or false
 	//does not specify the amount of items (if there are multiple copies)
-	boolean checkForItem(Item item) {
+	public boolean checkForItem(Item item) {
 		return inventory.checkForItem(item);
 	}
 	
 	//return the index of the inventory item specified
 	//if item does not exist, return -1
 	//if there are multiple copies of the item in the inventory, return the one with the lowest index
-	int indexOfItem(Item item) {
+	public int indexOfItem(Item item) {
 		return inventory.indexOfItem(item);
 	}
 	
 	//makes a string with the names of all items in the inventory
-	String inventoryToString() {
+	public String inventoryToString() {
 		return inventory.toString();
 	}
 
 	//same as above method, but the String lists two items as "itemName x2", instead of "itemName, itemName, etc."
 	//almost always more useful
-	String inventoryFancyToString() {
+	public String inventoryFancyToString() {
 		return inventory.fancyToString();
 	}
 
 	//returns the number of items in the inventory
-	int sizeOfInventory() {
+	public int sizeOfInventory() {
 		return inventory.sizeOfInventory();
+	}
+
+	public int sizeOfSubList(int subListIndex) {
+		return inventory.sizeOfSubList(subListIndex);
 	}
 	
 	//COMBAT
-	void attack(Animal target) {
+	public void attack(Animal target) {
 		//not done yet
 	}
 }

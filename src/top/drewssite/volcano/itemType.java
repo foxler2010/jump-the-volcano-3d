09f111c7c88050f4260d2ec0b84584b1690b61dc
foxler2010 <drew@drewssite.top>
2022-01-22 09:57:24 -0600

    I never added an "other" type.
    The misleading comment in itemType has been removed.

--- src/top/drewssite/volcano/itemType.java
+++ src/top/drewssite/volcano/itemType.java
@@ -2,7 +2,7 @@ package top.drewssite.volcano;
 
 //this is one way to find out what an item 'is'.
 //combined with the inheritance and abstraction of items, categorization should be very easy
-enum itemType {
+public enum itemType {
 	//below could be considered a roadmap of all types of items that are,
 	//or will be added to the game
 	MONSTER, //added!
@@ -11,5 +11,5 @@ enum itemType {
 	WEAPON,
 	JUNK, //added
 	FOOD,
-	OTHER //added!
+	OTHER
 }

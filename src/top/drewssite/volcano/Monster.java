e063048bf0e439241ba426abc7eb8a236fdcc103
foxler2010 <drew@drewssite.top>
2022-01-22 10:52:14 -0600

    Pet and Monster are now public.
    These should be the last of  the classes I need to make public.
    Now I think I am done cleaning the code, so I can start adding new features.

--- src/top/drewssite/volcano/Monster.java
+++ src/top/drewssite/volcano/Monster.java
@@ -3,7 +3,7 @@ package top.drewssite.volcano;
 //unfriendly animal.. pretty much exactly the same as pet
 class Monster extends Animal {
 
-	Monster(String name, int strength) {
+	public Monster(String name, int strength) {
 		//same as animal, but set type to itemType.MONSTER instead
 		super(name, itemType.MONSTER, strength, false);
 		
@@ -12,7 +12,7 @@ class Monster extends Animal {
 	
 	//would've made the target a Pet, but because of the abstract inheritance (i think that's what it is called)
 	//it has to be an Animal... so a monster can attack a monster if it wants
-	void attack(Animal target) {
+	public void attack(Animal target) {
 		//fighting mechanics are hard.. i'll do it later
 	}
 
--- src/top/drewssite/volcano/Pet.java
+++ src/top/drewssite/volcano/Pet.java
@@ -3,7 +3,7 @@ package top.drewssite.volcano;
 //friendly animal
 class Pet extends Animal {
 	
-	Pet(String name, int strength) {
+	public Pet(String name, int strength) {
 		//same as animal, but set type to itemType.PET instead
 		super(name, itemType.PET, strength, true);
 		
@@ -12,7 +12,7 @@ class Pet extends Animal {
 	
 	//would've made the target a Monster, but because of the abstract inheritance (i think that's what it is called)
 	//it has to be an Animal... so a pet can attack a pet if it wants
-	void attack(Animal target) {
+	public void attack(Animal target) {
 		//fighting mechanics are hard.. i'll do it later
 	}
 

ec75c2ef4af8b18532cd02b8f64edd36312df01c
foxler2010 <drew@drewssite.top>
2022-01-24 20:26:59 -0600

    getter and setter for Food.energy

--- src/top/drewssite/volcano/Food.java
+++ src/top/drewssite/volcano/Food.java
@@ -5,6 +5,16 @@ public class Food extends Junk {
     //VAR
     private int energy;
 
+    //GETTER AND SETTER
+	public double getEnergy() {
+		return energy;
+	}
+	
+	public void setEnergy(int energy) {
+		this.energy = energy;
+	}
+
+    //CONSTRUCTOR
     public Food(String name, double price, int energy) {
 
         super(name, price);
